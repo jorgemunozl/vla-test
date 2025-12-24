1102d75104662802c54c720326c40d80c197fdad
jorgemunozl <alvaro18ml@gmail.com>
2025-12-24 07:46:34 -0500

    fix pi05model embed

--- test/test_pi05_language.py
+++ test/test_pi05_language.py
@@ -2,12 +2,26 @@ import torch
 from transformers import AutoTokenizer
 from lerobot.policies.pi05.modeling_pi05 import PI05Policy
 
-# Load your policy as usual
-policy = PI05Policy.from_pretrained("lerobot/pi05_base")
-device = policy.config.device
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+policy = PI05Policy.from_pretrained("lerobot/pi05_base").to(device).eval()
 paligemma = policy.model.paligemma_with_expert.paligemma.to(device).eval()
 
-# Use the same tokenizer as PI05
+
+# Check that problematic wieght
+with torch.no_grad():
+    if hasattr(paligemma, "tie_weights"):
+        paligemma.tie_weights()
+
+    embed = paligemma.model.language_model.embed_tokens
+    lm_head = paligemma.lm_head
+    embed.weight = lm_head.weight
+
+print("tied", embed.weight.data_ptr() == lm_head.weight.data_ptr())
+
+
+# Tokenizer for Paligemma
 tokenizer = AutoTokenizer.from_pretrained("google/paligemma-3b-pt-224")
 
 prompt = "Hello there"
